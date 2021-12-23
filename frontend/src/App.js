f01a93fb2ebabfd8997c1bf1364eb72d11b7d505
Mégas Vanzillotta <megas.vanzi@gmail.com>
2021-12-22 22:51:20 -0300

    add provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Create React App Template.</p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Create React App Template.</p>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
