990ab008c7a48d449dc268fd06f9dc2da4b0d6ee
Mégas Vanzillotta <megas.vanzi@gmail.com>
2021-12-24 23:47:39 -0300

    enable router and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Create React App Template.</p>
-        </header>
-      </div>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
