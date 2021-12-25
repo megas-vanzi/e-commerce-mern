cfeef36f70d0534cc2b04409ea4248f8d389fdce
Mégas Vanzillotta <megas.vanzi@gmail.com>
2021-12-24 23:41:54 -0300

    create navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        height: "85px",
+        alignItems: "center",
+        justifyContent: "space-around",
+        margin: "10px 0px 20px",
+      }}
+    >
+      <div>
+        <Link to="/">
+          <div
+            style={{
+              width: "80px",
+              height: "40px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Home
+          </div>
+        </Link>
+
+        <Link to="/dashboard">
+          <div
+            style={{
+              width: "80px",
+              height: "40px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Dashboard
+          </div>
+        </Link>
+
+        <Link to="/about">
+          <div
+            style={{
+              width: "80px",
+              height: "40px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            About
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
