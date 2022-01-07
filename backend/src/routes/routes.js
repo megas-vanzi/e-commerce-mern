83f66de2ec6d659da51cf9d3c1d4a663c10eda3f
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-01-06 23:33:10 -0300

    create routes

---
+++ backend/src/routes/routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+// require
+// controllers
+
+router.post("/");
+router.get("/");
+router.put("/:id");
+router.delete("/:id");
+
+module.exports = router;
