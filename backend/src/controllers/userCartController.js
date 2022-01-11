f28988fc38a40cebf7c86e3e6f5255df79dd605b
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-01-10 22:00:30 -0300

    create user-cart controller

---
+++ backend/src/controllers/userCartController.js
@@ -0,0 +1,42 @@
+export default {
+  add: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  list: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  editById: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  deleteByName: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  addCart: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  listCart: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  editCart: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  deleteCart: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+};
