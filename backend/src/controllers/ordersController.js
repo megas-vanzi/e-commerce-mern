615caf2faee77c336a90d110bebd6bffde5e6899
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-01-10 21:58:46 -0300

    create orders controller

---
+++ backend/src/controllers/ordersController.js
@@ -0,0 +1,22 @@
+export default {
+  getOrders: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  getUserOrders: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  getOrder: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+  changeOrder: async (req, res, next) => {
+    try {
+      //TODO
+    } catch (e) {}
+  },
+};
