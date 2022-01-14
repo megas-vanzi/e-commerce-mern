3900aefe7b8700d947f11bb403a0e62fdcdcf9c2
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-01-14 13:14:45 -0300

    update orders model

--- backend/src/models/orders.js
+++ backend/src/models/orders.js
@@ -1,15 +1,50 @@
 import mongoose, { Schema } from "mongoose";
 
+const itemSchema = new Schema({
+  precio: {
+    type: Number,
+    trim: true,
+  },
+  productId: {
+    type: String,
+  },
+  cantidad: {
+    type: Number,
+    trim: true,
+  },
+});
+
 const orderSchema = new Schema({
   user: {
     type: String,
   },
-  order: [
-    {
-      type: Schema.ObjectId,
-      ref: "cart",
-    },
-  ],
+  order: [itemSchema],
+  total: {
+    type: Number,
+  },
+  envio: {
+    type: Boolean,
+  },
+  domicilio: {
+    type: String,
+  },
+  estado: {
+    type: String,
+    default: "creada",
+  },
+  creada: {
+    type: Boolean,
+    default: true,
+  },
+  pagada: {
+    type: Boolean,
+  },
+  enviada: {
+    type: Boolean,
+  },
+  recibida: {
+    type: Boolean,
+  },
 });
 
 const Order = mongoose.model("order", orderSchema);
