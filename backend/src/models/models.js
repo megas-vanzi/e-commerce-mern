91d906f20c72181f0b140b7e7461980971c0d6df
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-01-29 21:49:53 -0300

    enable new models

--- backend/src/models/models.js
+++ backend/src/models/models.js
@@ -1,9 +1,15 @@
 import User from "./users";
 import Cart from "./cart";
 import Order from "./orders";
+import Category from "./categories";
+import Product from "./products";
+import Review from "./reviews";
 
 export default {
   User,
   Cart,
   Order,
+  Category,
+  Product,
+  Review,
 };
