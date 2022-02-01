cb7abef298368adfd5cf830959f6c5c6bae4e203
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-02-01 00:09:13 -0300

    add new routes to router

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,6 +3,9 @@ import authRouter from "./authRoutes";
 import usersRouter from "./usersRoutes";
 import cartRouter from "./cartRoutes";
 import ordersRouter from "./ordersRoutes";
+import categoriesRouter from "./categoriesRoutes";
+import productsRouter from "./productsRoutes";
+import reviewsRouter from "./reviewsRoutes";
 
 const router = routerx();
 
@@ -13,5 +16,11 @@ router.use("/users", usersRouter);
 router.use("/cart", cartRouter);
 
 router.use("/orders", ordersRouter);
+
+router.use("/categories", categoriesRouter);
+
+router.use("/products", productsRouter);
+
+router.use("/reviews", reviewsRouter);
 
 export default router;
