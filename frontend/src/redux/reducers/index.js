1bcb5d6bed1badbfd0c8841aba72a15c19ef1474
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-02-16 12:15:50 -0300

    combine new reducers

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cartReducer";
+import categoriesReducer from "./categoriesReducer";
 import ordersReducer from "./ordersReducer";
 import productsReducer from "./productsReducer";
+import reviewsReducer from "./reviewsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
   cart: cartReducer,
   orders: ordersReducer,
   products: productsReducer,
+  categories: categoriesReducer,
+  reviews: reviewsReducer,
+  users: usersReducer,
 });
