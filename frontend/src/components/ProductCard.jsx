39b26610078fa053f0795a3b422fe263be42e3a5
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-02-26 21:19:55 -0300

    update products

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({ _id, name, price, images, stock }) => {
+  return (
+    <div>
+      <h4>{name}</h4>
+      <h5>{price}</h5>
+      <h5>Stock: {stock}</h5>
+      {images?.map((image) => (
+        <img key={image} src={image} alt={name} width="100px" />
+      ))}
+      <button onClick={() => addToCart(_id)}>Add to Cart</button>
+    </div>
+  );
 };
 
 export default ProductCard;
--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import { useSelector } from "react-redux";
 
 const Products = () => {
+  const products = useSelector((state) => state.products.products);
+
   return (
     <div>
+      {products.map(({ _id, name, price, images, stock }) => {
+        return (
+          <LastTenItems
+            key={_id}
+            _id={_id}
+            name={name}
+            price={price}
+            images={images}
+            stock={stock}
+          />
+        );
+      })}
       Products
       <ProductCard />
     </div>
